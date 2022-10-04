81dea017da0f569eea2c640ebdfa8ad43b5456cd
ws212 <wenshengl@gmail.com>
2022-10-04 14:50:46 -0400

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="tic">
+                <Board />
+            </div>
+        );
+    }
+}
+export default App;
